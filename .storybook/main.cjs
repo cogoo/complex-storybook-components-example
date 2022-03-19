19e7bc72e6e5e1899495874136a64161ce5d157c
Colin Ogoo <ogoo.colin@gmail.com>
2022-03-19 19:39:59 +0000

    build(s): storybook tailwind config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,6 +1,26 @@
+const path = require('path');
+
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-svelte-csf',
+		{
+			name: '@storybook/addon-postcss',
+			options: {
+				cssLoaderOptions: {
+					// When you have splitted your css over multiple files
+					// and use @import('./other-styles.css')
+					importLoaders: 1
+				},
+				postcssLoaderOptions: {
+					// When using postCSS 8
+					implementation: require('postcss')
+				}
+			}
+		}
+	],
 	framework: '@storybook/svelte',
 	svelteOptions: {
 		preprocess: require('svelte-preprocess')
