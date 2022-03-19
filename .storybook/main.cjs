17d77e835ac290f1408767f8ff94f171bc667fdc
Colin Ogoo <ogoo.colin@gmail.com>
2022-03-19 15:38:44 +0000

    fix: broken storybook esm imports

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,15 +1,18 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-svelte-csf"
-  ],
-  "framework": "@storybook/svelte",
-  "svelteOptions": {
-    "preprocess": require('svelte-preprocess')
-  }
-}
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
+	framework: '@storybook/svelte',
+	svelteOptions: {
+		preprocess: require('svelte-preprocess')
+	},
+	webpackFinal: async (config, { configType }) => {
+		config.module.rules.push({
+			test: /\.mjs$/,
+			include: /node_modules/,
+			type: 'javascript/auto'
+		});
+
+		// Return the altered config
+		return config;
+	}
+};
